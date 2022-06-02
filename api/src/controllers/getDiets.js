bd2072b49f27a86aa9f10775bbca1e4922175820
CRISTIAN0026 <92878127+CRISTIAN0026@users.noreply.github.com>
2022-06-02 14:11:48 -0500

    firts commit

--- api/src/controllers/getDiets.js
+++ api/src/controllers/getDiets.js
@@ -1,17 +1,17 @@
 const dietTypesDb = [
-'gluten free', 
-'ketogenic', 
-'vegetarian', 
-'lacto vegetarian',
-'ovo vegetarian', 
-'lacto ovo vegetarian', 
-'vegan', 
-'pescetarian', 
-'paleolithic', 
-'primal', 
-'low fodmap', 
-'whole 30', 
-'dairy free'];
+"gluten free", 
+"ketogenic", 
+"vegetarian", 
+"lacto vegetarian",
+"ovo vegetarian", 
+"lacto ovo vegetarian", 
+"vegan", 
+"pescetarian", 
+"paleolithic", 
+"primal", 
+"low fodmap", 
+"whole 30", 
+"dairy free"];
 
 module.exports = {
     dietTypesDb
