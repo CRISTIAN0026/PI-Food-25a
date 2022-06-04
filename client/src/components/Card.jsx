81734b390f37db0851388f718ca8c9f1a7a6c39f
CRISTIAN0026 <92878127+CRISTIAN0026@users.noreply.github.com>
2022-06-04 10:36:56 -0500

    firts commit

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,8 +5,8 @@ export default function Card ({name, image, diets}) {
     return(
         <div id="card1">
             <h3 id="h3">{name}</h3>
-            <h5>{diets}</h5>
-            <img src={image} alt="not found" width='300px' height='350px'/>
+            <h5 id="h5">{diets}</h5>
+            <img id="img" src={image} alt="not found" width='300px' height='300px'/>
         </div>
     )
 }
